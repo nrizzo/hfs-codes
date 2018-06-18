b55cc5c43d86ac5e192914a83ab652422c8d8741
nrizzo <nrizzo@users.noreply.github.com>
2018-06-18 11:57:29 +0200

    Funzioni relative alla codifica di Ackermann

---
+++ src/toolbox/functions.hs
@@ -0,0 +1,40 @@
+-- Funzioni utili
+
+tobinrep :: Int -> [Char]
+tobinrep 0 = "0"
+tobinrep 1 = "1"
+tobinrep x
+    | x `mod` 2 == 0 = (tobinrep (x `div` 2)) ++ ['0']
+    | otherwise      = (tobinrep (x `div` 2)) ++ ['1']
+
+-- AND tra due stringhe --
+binrepAND :: [Char] -> [Char] -> [Char]
+binrepAND x y = reverse (binrepANDaux (reverse x) (reverse y)) 
+
+binrepANDaux :: [Char] -> [Char] -> [Char]
+binrepANDaux x [] = x
+binrepANDaux [] y = y
+binrepANDaux (x:xs) (y:ys) = [cAND x y] ++ binrepANDaux xs ys
+ where cAND '1' _ = '1'
+       cAND _ '1' = '1'
+       cAND _  _  = '0'
+
+-- per trovare la "maschera" che rappresenta i nodi da calcolare
+hfsmask :: Int -> [Char]
+hfsmask x = hfsmaskrec b ((length b) - 1)
+ where b = tobinrep x
+
+hfsmaskrec :: [Char] -> Int -> [Char]
+hfsmaskrec [] _ = []
+hfsmaskrec (s:[]) _ = [s]
+hfsmaskrec (s:xs) i
+    | s == '1'  = [s] ++ (hfsmaskrec (binrepAND xs (tobinrep i)) (i-1))
+    | otherwise = [s] ++ (hfsmaskrec xs (i-1))
+
+
+onescount :: [Char] -> Int
+onescount [] = 0
+onescount ('1':xs) = 1 + onescount xs
+onescount (_:xs) = onescount xs
+
+-- [ onescount (hfsmask x) | x <- [0..50] ] --
--- src/toolbox/functions.scm
+++ src/toolbox/functions.scm
@@ -1,13 +1,34 @@
 ;; Funzioni utili
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname useful_functions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (define number->bin-rep ;val: string
   (lambda (n) ;n: natural
-    (cond ((= n 1) "1")
+    (cond ((= n 0) "0")
+          ((= n 1) "1")
           ((= (modulo n 2) 0) (string-append (number->bin-rep (quotient n 2)) "0"))
           (else (string-append (number->bin-rep (quotient n 2)) "1"))
 )))
+(define bin->int ; val: int
+  (lambda (s) ; s: stringa
+    (cond ((string=? s "") 0)
+          ((char=? (last-char s) #\0) (* 2 (bin->int (string-split s))))
+          (else (+ 1 (* 2 (bin->int (string-split s)))))
+)))
+(define string-split ; val: stringa
+  (lambda (s) ; s: stringa
+    (substring s 0 (- (string-length s) 1))
+))
+(define last-char ; val: char
+  (lambda (p) ; p: stringa
+    (string-ref p (- (string-length p) 1))
+))
+
+(define ones-count
+  (lambda (s)
+    (cond ((= (string-length s) 0) 0)
+          ((char=? #\1 (string-ref s 0)) (+ 1 (ones-count (substring s 1))))
+          (else (ones-count (substring s 1)))
+)))
+
+
 
 (define tetrazione2 ;val: natural
   (lambda (n) ;n: natural
@@ -50,3 +71,54 @@
         (number->bin-rep x)
         (highlight-prefix (number->bin-rep x) (highlight-prefix-between (+ x 1) y)))
 ))
+
+(define bin-rep-and
+  (lambda (s1 s2)
+    (let ((l1 (string-length s1))
+          (l2 (string-length s2)))
+      (cond ((= l1 0) s2)
+            ((= l2 0) s1)
+            ((> l1 l2) (string-append (substring s1 0 1) (bin-rep-and (substring s1 1) s2)))
+            ((< l1 l2) (string-append (substring s2 0 1) (bin-rep-and s1 (substring s2 1))))
+            ((or (char=? #\1 (string-ref s1 0)) (char=? #\1 (string-ref s2 0)))
+             (string-append "1" (bin-rep-and (substring s1 1) (substring s2 1))))
+            (else
+             (string-append "0" (bin-rep-and (substring s1 1) (substring s2 1))))
+))))
+
+(define hfs-mask
+  (lambda (i)
+    (let ((s (number->bin-rep i)))
+      (hfs-mask-rec s 0 (- (string-length s) 1))
+)))
+
+(define hfs-mask-rec
+  (lambda (s i j)
+    (cond ((= j 0) s)
+          ((char=? #\1 (string-ref s i)) (hfs-mask-rec (bin-rep-and s (number->bin-rep j)) (+ i 1) (- j 1)))
+          (else (hfs-mask-rec s (+ i 1) (- j 1))))
+))
+
+(define hfs-mask-between
+  (lambda (i1 i2)
+    (if (<= i1 i2)
+        (cons (hfs-mask i1) (hfs-mask-between (+ i1 1) i2))
+        '()
+)))
+
+(define show-list
+  (lambda (l)
+    (if (null? l)
+        ""
+        (string-append (number->string (car l)) ", " (show-list (cdr l)))
+)))
+
+(define uniq
+  (lambda (l)
+    (cond ((null? l) null)
+          ((= (length l) 1) l)
+          ((= (car l) (cadr l)) (uniq (cdr l)))
+          (else (cons (car l) (uniq (cdr l))))
+)))
+
+;(show-list (map ones-count (hfs-mask-between 0 50)))
